2cc0d162d2dd0c68e02950feaa34b70df07914c7
Yemi Alade <ylawal7@gmail.com>
2024-07-30 11:05:31 +0000

    Fix division function

--- assets/js/script.js
+++ assets/js/script.js
@@ -146,6 +146,6 @@ function displayDivisionQuestion(operand1, operand2) {
 operand1 = operand1 * operand2;
 
     document.getElementById("operand1").textContent = operand1 
-    document.getElementById("operand2").textContent = operand1 
+    document.getElementById("operand2").textContent = operand2 
     document.getElementById('operator').textContent = "/";
 }
